466ccc783b31ea919e5d696661d64b345e2658f4
James Spencer <jspenceratty@outlook.com>
2021-08-03 09:54:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -297,19 +297,3 @@ function addNewCD() {
 }
 
 DOMManager.getAllCDs();
-
-function forceRedraw(element){
-
-    if (!element) { return; }
-
-    var n = document.createTextNode(' ');
-    var disp = element.style.display;  // don't worry about previous display style
-
-    element.appendChild(n);
-    element.style.display = 'none';
-
-    setTimeout(function(){
-        element.style.display = disp;
-        n.parentNode.removeChild(n);
-    },20); // you can play with this timeout to make it as short as possible
-}
